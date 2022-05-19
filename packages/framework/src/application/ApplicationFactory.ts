19ee67d4729ead39fe740f8644ae5771f1d1ad09
Bailey Herbert <hello@bailey.sh>
2022-05-19 14:00:28 -0700

    Add a factory to instantiate application instances

---
+++ packages/framework/src/application/ApplicationFactory.ts
@@ -0,0 +1,28 @@
+import { Container } from '@baileyherbert/container';
+import { Constructor } from '@baileyherbert/types';
+import { Application } from './Application';
+
+class ApplicationFactoryImpl {
+
+	/**
+	 * Creates a new instance of the given application class.
+	 *
+	 * @param constructor
+	 * @returns
+	 */
+	public create<T extends Application>(constructor: Constructor<T>): T {
+		const container = new Container();
+		const instance = container.resolve(constructor);
+
+		container.register(Application, { useValue: instance });
+		container.registerInstance(instance);
+
+		return instance;
+	}
+
+}
+
+/**
+ * A factory used to create new application instances.
+ */
+export const ApplicationFactory = new ApplicationFactoryImpl();
